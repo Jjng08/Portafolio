84b744ccc931488a5c9dbd5c77c3077d491a2ad9
Epic <byepic@outlook.es>
2025-03-07 12:47:55 -0300

    add animations badge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+import animations from '@midudev/tailwind-animations'
+
 export default {
   content: [
     './src/**/*.{astro,html,js,jsx,ts,tsx}',
   ],
   darkMode: 'class', 
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        pulsing: {
+          '0%, 100%': { opacity: '1' },
+          '50%': { opacity: '0.5' },
+        },
+      },
+      animation: {
+        'pulsing': 'pulsing 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      }
+    },
   },
-  plugins: [],
+  plugins: [animations],
 }
 
